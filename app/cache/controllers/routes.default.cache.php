540dc476d7d4df56ee4352145c80696497d2aaea
Babstonks <22008106@etu.unicaen.fr>
2022-03-24 15:59:19 +0100

    B - route par défaut

---
+++ app/cache/controllers/routes.default.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("/_default/"=>["controller"=>"controllers\\StoreController","action"=>"index","parameters"=>[],"name"=>"store.index","cache"=>false,"duration"=>0]);
---
+++ app/controllers/StoreController.php
@@ -0,0 +1,27 @@
+<?php
+namespace controllers;
+ use models\Product;
+ use models\Section;
+ use Ubiquity\attributes\items\router\Route;
+ use Ubiquity\orm\DAO;
+ use Ubiquity\orm\repositories\ViewRepository;
+
+ /**
+  * Controller StoreController
+  */
+class StoreController extends \controllers\ControllerBase{
+    private ViewRepository $repo;
+
+    public function initialize() {
+        parent::initialize();
+        $this->repo??=new ViewRepository($this,Section::class);
+    }
+
+    #[Route('_default')]
+	public function index(){
+        $this->repo->all(condition: "",included: ["products"]);
+        $count=DAO::count(Product::class);
+
+        $this->loadView('StoreController/index.html', ['countProduct' => $count]);
+	}
+}
