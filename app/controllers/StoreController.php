1495773a789241ebe10d3641071c51cc2130f003
Babstonks <22008106@etu.unicaen.fr>
2022-03-24 18:51:07 +0100

    Commit D & E & final

--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -3,9 +3,11 @@ namespace controllers;
  use models\Product;
  use models\Section;
  use Ubiquity\attributes\items\router\Get;
+ use Ubiquity\attributes\items\router\Post;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\orm\DAO;
  use Ubiquity\orm\repositories\ViewRepository;
+ use Ubiquity\utils\http\USession;
 
  /**
   * Controller StoreController
@@ -14,8 +16,10 @@ class StoreController extends \controllers\ControllerBase{
     private ViewRepository $repo;
 
     public function initialize() {
+        $this->view->setVar("test", 0);
         parent::initialize();
         $this->repo??=new ViewRepository($this,Section::class);
+
     }
 
     #[Route('_default')]
@@ -31,4 +35,17 @@ class StoreController extends \controllers\ControllerBase{
         $this->repo->byId($idSection, ['products']);
         $this->loadView('StoreController/section.html');
     }
+
+    #[Get(path: "store/products",name: "store.products")]
+    public function allProducts(){
+        $count=DAO::getAll(Product::class);
+        $this->loadView('StoreController/products.html', compact('count'));
+    }
+
+    #[Get(path: "store/addToCart/{idProduct}/{count}",name: "store.addToCart")]
+    public function addToCart(int $idProduct, int $count){
+        $session = USession::get($idProduct);
+        $this->loadView('StoreController/addToCart.html');
+    }
+
 }
